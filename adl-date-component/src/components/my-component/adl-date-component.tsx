62eb063930462b1e13dcdad40798755034353a31
Luis Felipe Becerra <luisbecerra.424@gmail.com>
2019-09-13 11:19:29 -0500

    get date from inputs

--- adl-date-component/src/components/my-component/adl-date-component.tsx
+++ adl-date-component/src/components/my-component/adl-date-component.tsx
@@ -8,19 +8,73 @@ import moment from 'moment';
 })
 export class AdlDateComponent {
 
-  @Prop() year: string;
-  @Prop() month: string;
-  @Prop() day: string;
+	private year: number;
+	private month: number;
+	private day: number;
+
+	@Prop({ reflect: true }) value: string;
+	@Prop() format: string;
+
+	handleKey(evt: KeyboardEvent, dtType: number){
+		let el = null;
+		let value = null;
+
+		// GET ELEMENT
+		// FIX FOR FIREFOX AND SAFARI
+		if(evt.path){
+			el = evt.path[0];
+		} else if(evt.explicitOriginalTarget) {
+			el = evt.explicitOriginalTarget;
+		}
+
+		// CHECK IF VALUE IS A NUMBER
+		if(isNaN(el.value)) {
+			el.value = null;
+			return;
+		}
+
+		if(dtType === 3) {
+			this.year = el.value;
+		} else if(dtType === 2) {
+			this.month = el.value;
+		} else if(dtType === 1) {
+			this.day = el.value;
+		}
+
+		let date = `${this.year}/${this.month}/${this.day}`;
+		console.log(date)
+		this.value = date;
+	}
 
   render() {
-		let date = moment().format();
-		console.log(date);
+	
+
+		console.log(this.format);
+		var aDate     = moment(this.value);
+		var invalidAt = aDate.invalidAt();
+		console.log(invalidAt);
+		console.log(aDate.format(this.format));
 
     return (
       <div>
-        <input type="text" id="year"/>
-        <input type="text" id="month"/>
-        <input type="text" id="day"/>
+        <input 
+					type="tel"
+					value={this.year}
+					onkeyup={($evt) => this.handleKey($evt,3)} 
+				/>
+
+        <input
+					type="tel"
+					value={this.month}
+					onkeyup={($evt) => this.handleKey($evt,2)} 
+				/>
+
+        <input
+					type="tel"
+					value={this.day}
+					onkeyup={($evt) => this.handleKey($evt,1)}
+				/>
+
       </div>
     );
   }
