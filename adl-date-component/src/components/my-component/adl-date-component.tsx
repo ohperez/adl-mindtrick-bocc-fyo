8f7a670b16cded81220d9b876a89fe6993cf266a
Luis Felipe Becerra <luisbecerra.424@gmail.com>
2019-09-13 17:13:11 -0500

    show popups

--- adl-date-component/src/components/my-component/adl-date-component.tsx
+++ adl-date-component/src/components/my-component/adl-date-component.tsx
@@ -2,11 +2,10 @@ import { Component, Prop, h } from '@stencil/core';
 import moment from 'moment';
 import { 
 	getElementFromEvt,
-	validateYear,
-	validateMonth,
-	validateDay, 
 	disableInputs,
-	setValueFromEvt
+	setValueFromEvt,
+	clearOnValidate,
+	validateDate
 } from '../../utils/utils';
 
 @Component({
@@ -26,10 +25,18 @@ export class AdlDateComponent {
 	private day: number;
 	private yearsList = [];
 
+	public inputYear = null;
+	public inputMonth = null;
+	public inputDay = null;
+
 	componentWillLoad() {
     for (let i = 1900; i < 2019; i++) {
-    this.yearsList.push(i);
-   }
+			this.yearsList.push(i);
+		}
+
+		this.inputYear = this.cmptEl.shadowRoot.querySelector('[dtType="year"]');
+		this.inputMonth = this.cmptEl.shadowRoot.querySelector('[dtType="month"]');
+		this.inputDay = this.cmptEl.shadowRoot.querySelector('[dtType="day"]');
   }
 
 	handleKey(evt: KeyboardEvent, dtType: number){
@@ -40,24 +47,64 @@ export class AdlDateComponent {
 		let { year, month, day } = setValueFromEvt(
 				el, dtType, this.year, this.month, this.day
 		)
-		
+
+		disableInputs(this.cmptEl, year,month, day);
+		clearOnValidate(this.cmptEl, year, month, day);
+
 		this.year = year;
 		this.month = month;
 		this.day = day;
-
-		disableInputs(this.cmptEl, this.year,this.month, this.day);
 		
-		let date = `${this.year}-${this.month}-${this.day}`;
-		let odate = moment(date, 'YYYY-MM-DD', true);
-		console.log(date, odate.invalidAt());
+		if(validateDate(this.year,this.month,this.date)){
+			let date = `${this.year}-${this.month}-${this.day}`;
+			let odate = moment(date);
+			console.log(odate.format(this.format));
+		}
+	}
 
-		this.value = date;
+	onYearClick(clickYear: number){
+		this.inputYear = this.cmptEl.shadowRoot.querySelector('[dtType="year"]');
+		this.inputMonth = this.cmptEl.shadowRoot.querySelector('[dtType="month"]');
+		this.year = clickYear;
+		this.inputYear.value = clickYear;
+		
+		disableInputs(this.cmptEl, this.year, this.month, this.day);
+		this.inputMonth.focus();
 
+		if(validateDate(this.year,this.month,this.date)){
+			let date = `${this.year}-${this.month}-${this.day}`;
+			let odate = moment(date);
+			console.log(odate.format(this.format));
+		}
+	}
+
+	showPopup(evt: Event, dtType: number){
+		console.log(dtType)
+		let popup = this.cmptEl.shadowRoot.querySelector('.popup-container');
+		let popupYear = this.cmptEl.shadowRoot.querySelector('.years-container');
+		let popupMonth = this.cmptEl.shadowRoot.querySelector('.months-container');
+		let popupDay = this.cmptEl.shadowRoot.querySelector('.days-container');
+		
+		if(dtType === 3){
+			popupYear.classList.add('show');
+			popupMonth.classList.remove('show');
+			popupDay.classList.remove('show');
+		} else if(dtType === 2){
+			popupYear.classList.remove('show');
+			popupMonth.classList.add('show');
+			popupDay.classList.remove('show');
+		} else if(dtType === 1){
+			popupYear.classList.remove('show');
+			popupMonth.classList.remove('show');
+			popupDay.classList.add('show');
+		}
+
+		popup.classList.add('show');
 	}
 
 	renderList(item) {
     return (
-      <div class="grid-element-year">
+      <div onClick={() => this.onYearClick(item)} class="grid-element-year">
         <span>{item}</span>
       </div>
 		);
@@ -74,6 +121,7 @@ export class AdlDateComponent {
 						class="year-input"
 						value={this.year}
 						onkeyup={($evt) => this.handleKey($evt,3)}
+						onfocus={($evt) => this.showPopup($evt,3)}
 						maxlength="4"
 						dtType="year"
 					/>
@@ -83,6 +131,7 @@ export class AdlDateComponent {
 						type="tel"
 						value={this.month}
 						onkeyup={($evt) => this.handleKey($evt,2)}
+						onfocus={($evt) => this.showPopup($evt,2)}
 						dtType="month"
 						maxlength="2"
 						disabled
@@ -94,6 +143,7 @@ export class AdlDateComponent {
 						type="tel"
 						value={this.day}
 						onkeyup={($evt) => this.handleKey($evt,1)}
+						onfocus={($evt) => this.showPopup($evt,1)}
 						dtType="day"
 						maxlength="2"
 						disabled
@@ -108,7 +158,6 @@ export class AdlDateComponent {
             <div class="grid-container">
               {
                 this.yearsList.map(item => {
-                  console.log(item);
                   return this.renderList(item);
                 })
               }
